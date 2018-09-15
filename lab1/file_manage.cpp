89922841541afda3eb3e8fe90e9d6e609394fe80
AJIOB <aleksandr9809@gmail.com>
2018-09-15 19:42:17 +0300

    Server upload prototype was written

--- lab1/file_manage.cpp
+++ lab1/file_manage.cpp
@@ -4,9 +4,56 @@
 
 std::string execute_upload(custom_sock_t s, std::string& params)
 {
-	//TODO:
-	std::string temp("TODO: upload");
-	return append_newline(temp);
+	std::string res = "Completed";
+	std::string file_name = FILE_SHARED_FOLDER + params;
+	
+	send_recv_payload_t to_recv;
+	file_session_t session;
+	session.next_part_to_send_recv = 0;
+	session.current_operation = file_operation_upload;
+	session.handle = fopen(file_name.c_str(), "wb");
+	if (!(session.handle))
+	{
+		res = "Cannot open output file";
+		return append_newline(res);
+	}
+
+	std::vector<uint8_t> recv_buff;
+
+	do
+	{
+		UPLOAD_DOWNLOAD_SIZE_TYPE recv_size = 0;
+		if (recv(s, (char*)&recv_size, sizeof(recv_size), 0) != sizeof(recv_size))
+		{
+			fprintf(stderr, "Cannot receive packet size\r\n");
+			res = "Error receiving size";
+			break;
+		}
+		recv_buff.assign(recv_size, 0);
+		if (recv(s, (char*)recv_buff.data(), recv_size, 0) != recv_size)
+		{
+			fprintf(stderr, "Cannot receive packet\r\n");
+			res = "Error receiving packet";
+			break;
+		}
+
+		if (!payload_struct_deserialize(to_recv, recv_buff, recv_size))
+		{
+			fprintf(stderr, "Cannot deserialize packet\r\n");
+			res = "Error deserialize packet";
+			break;
+		}
+
+		if (!file_write(session, to_recv))
+		{
+			res = "Error writing";
+			break;
+		}
+	} while (!(to_recv.is_last));
+
+	fclose(session.handle);
+
+	return append_newline(res);
 }
 
 std::string execute_download(custom_sock_t s, std::string& params)
@@ -15,8 +62,8 @@ std::string execute_download(custom_sock_t s, std::string& params)
 	std::string file_name = FILE_SHARED_FOLDER + params;
 	send_recv_payload_t to_send;
 	file_session_t session;
-	session.next_part_to_send = 0;
-	session.current_operation = file_operation_upload;
+	session.next_part_to_send_recv = 0;
+	session.current_operation = file_operation_download;
 	session.handle = fopen(file_name.c_str(), "rb");
 	if (!(session.handle))
 	{
@@ -78,7 +125,7 @@ bool file_read(file_session_t& session, send_recv_payload_t& to_send)
 	to_send.sector_amount = ((file_len - 1) / kBlockSize + 1);
 	printf("Out file len: %ld bytes\r\n", file_len);
 
-	if (fseek(session.handle, (long)(kBlockSize * session.next_part_to_send), SEEK_SET))
+	if (fseek(session.handle, (long)(kBlockSize * session.next_part_to_send_recv), SEEK_SET))
 	{
 		fprintf(stderr, "Read: cannot seek the file\n");
 		return false;
@@ -89,10 +136,33 @@ bool file_read(file_session_t& session, send_recv_payload_t& to_send)
 	to_send.payload.reserve(were_read);
 	to_send.payload.insert(to_send.payload.end(), buff, buff + were_read);
 
-	to_send.sector_current_num = session.next_part_to_send;
-	session.next_part_to_send++;
+	to_send.sector_current_num = session.next_part_to_send_recv;
+	session.next_part_to_send_recv++;
 
 	to_send.is_last = ((to_send.sector_current_num + 1) >= to_send.sector_amount);
+
+	return true;
+}
+
+bool file_write(file_session_t& session, send_recv_payload_t& to_recv)
+{
+	static const uint64_t kBlockSize = SERIALIZER_MAX_PAYLOAD_SIZE;
+
+	if (fseek(session.handle, (long)(kBlockSize * to_recv.sector_current_num), SEEK_SET))
+	{
+		fprintf(stderr, "Write: cannot seek the file\n");
+		return false;
+	}
+
+	const size_t were_write = fwrite(to_recv.payload.data(), sizeof(to_recv.payload[0]), 
+		to_recv.payload.size(), session.handle);
+	if (were_write != to_recv.payload.size())
+	{
+		fprintf(stderr, "Were written %llu bytes instead of %llu\r\n", were_write, to_recv.payload.size());
+		return false;
+	}
+
+	session.next_part_to_send_recv++;
 
 	return true;
 }
--- lab1/file_manage.h
+++ lab1/file_manage.h
@@ -19,7 +19,7 @@ typedef struct file_session_t
 {
 	FILE* handle;
 	file_operation_t current_operation;
-	uint64_t next_part_to_send;
+	uint64_t next_part_to_send_recv;
 } file_session_t;
 
 std::string execute_upload(custom_sock_t s, std::string& params);
@@ -29,3 +29,4 @@ std::string execute_continue_download(custom_sock_t s, std::string& params);
 std::string append_newline(std::string& s);
 
 bool file_read(file_session_t& session, send_recv_payload_t& to_send);
+bool file_write(file_session_t& session, send_recv_payload_t& to_recv);
