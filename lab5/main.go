10e72c3516c3ab477747e50e6791c354c4afb8ed
AJIOB <aleksandr9809@gmail.com>
2018-11-04 13:24:26 +0300

    lab5 ping by library was implemented

---
+++ lab5/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tatsushid/go-fastping"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	//fmt.Println("hello world")
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
