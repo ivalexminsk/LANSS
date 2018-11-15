2fa680119b5730581d1db406ee608505f5330b10
AJIOB <aleksandr9809@gmail.com>
2018-11-15 18:57:48 +0300

    Broadcast config & its reading were added

--- lab6/main.go
+++ lab6/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	type Config struct {
+		Mode          string `json:"mode"`
+		MulticastAddr string `json:"multicast_addr"`
+		Port          int    `json:"net_port"`
+	}
+
+	if len(os.Args) < 2 {
+		log.Fatal("Set config file path in arg 1")
+	}
+
+	content, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Println("Cannot read config file")
+		log.Fatal(err)
+	}
+
+	c := Config{}
+	json.Unmarshal(content, &c)
+
+	appNeedClose := make(chan os.Signal, 1)
+	signal.Notify(appNeedClose, os.Interrupt)
+
+	switch c.Mode {
+	case "multicast":
+	case "broadcast":
+	default:
+		log.Fatal("Unsupported mode", c.Mode)
+	}
 }
